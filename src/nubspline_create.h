4795549da62b6c9d753ecce5fb5a97546c2d8283
esler <esler@d3ca5a85-aa2c-0410-a728-ae544c1d3856>
2007-04-11 17:01:58 +0000

    Added structures for nonuniform 3D basis splines.

--- src/nubspline_create.h
+++ src/nubspline_create.h
@@ -21,22 +21,56 @@ create_general_grid (double *points, int num_points);
 NUBspline_1d_s *
 create_NUBspline_1d_s (NUgrid* restrict x_grid, BCtype_s xBC, float *data);
 
+NUBspline_2d_s *
+create_NUBspline_2d_s (NUgrid* restrict x_grid, NUgrid* restrict y_grid,
+		       BCtype_s xBC, BCtype_s yBC, float *data);
+
+NUBspline_3d_s *
+create_NUBspline_3d_s (NUgrid* restrict x_grid, NUgrid* restrict y_grid, NUgrid* restrict z_grid,
+		       BCtype_s xBC, BCtype_s yBC, BCtype_s zBC, float *data);
+
 ////////////////////////////////////////
 // Nonuniform, double precision, real //
 ////////////////////////////////////////
 NUBspline_1d_d *
-create_NUBspline_1d_d (NUgrid* restrict x_grid, BCtype_d xBC, float *data);
+create_NUBspline_1d_d (NUgrid* restrict x_grid, BCtype_d xBC, double *data);
+
+NUBspline_2d_d *
+create_NUBspline_2d_d (NUgrid* restrict x_grid, NUgrid* restrict y_grid, 
+		       BCtype_d xBC, BCtype_d yBC, double *data);
+
+NUBspline_3d_d *
+create_NUBspline_3d_d (NUgrid* restrict x_grid, NUgrid* restrict y_grid, NUgrid* restrict z_grid, 
+		       BCtype_d xBC, BCtype_d yBC, BCtype_d zBC, double *data);
 
 ///////////////////////////////////////////
 // Nonuniform, single precision, complex //
 ///////////////////////////////////////////
 NUBspline_1d_c *
-create_NUBspline_1d_c (NUgrid* restrict x_grid, BCtype_c xBC, float *data);
+create_NUBspline_1d_c (NUgrid* restrict x_grid, BCtype_c xBC, 
+		       complex_float *data);
+
+NUBspline_2d_c *
+create_NUBspline_2d_c (NUgrid* restrict x_grid, NUgrid* restrict y_grid, 
+		       BCtype_c xBC, BCtype_c yBC, complex_float *data);
+
+NUBspline_3d_c *
+create_NUBspline_3d_c (NUgrid* restrict x_grid, NUgrid* restrict y_grid, NUgrid* restrict z_grid, 
+		       BCtype_c xBC, BCtype_c yBC, BCtype_c zBC, 
+		       complex_float *data);
 
 ///////////////////////////////////////////
 // Nonuniform, double precision, complex //
 ///////////////////////////////////////////
 NUBspline_1d_z *
-create_NUBspline_1d_z (NUgrid* restrict x_grid, BCtype_z xBC, float *data);
+create_NUBspline_1d_z (NUgrid* restrict x_grid, BCtype_z xBC, 
+		       complex_double *data);
+NUBspline_2d_z *
+create_NUBspline_2d_z (NUgrid* restrict x_grid, NUgrid* restrict y_grid, 
+		       BCtype_z xBC, BCtype_z yBC, complex_double *data);
+
+NUBspline_3d_z *
+create_NUBspline_3d_z (NUgrid* restrict x_grid, NUgrid* restrict y_grid, NUgrid* restrict z_grid, 
+		       BCtype_z xBC, BCtype_z yBC, BCtype_z zBC, complex_double *data);
 
 #endif
--- src/nubspline_structs.h
+++ src/nubspline_structs.h
@@ -20,8 +20,8 @@ typedef struct
 {
   spline_code code;
   float* restrict coefs;
-  NUgrid *x_grid;
-  NUBasis *x_basis;
+  NUgrid *restrict  x_grid;
+  NUBasis *restrict x_basis;
   BCtype_s xBC;
 } NUBspline_1d_s;
 
@@ -30,10 +30,20 @@ typedef struct
   spline_code code;
   float* restrict coefs;
   int x_stride;
-  NUgrid *x_grid, *y_grid;
-  NUBasis *x_basis, *y_basis;
+  NUgrid  *restrict x_grid,  *restrict y_grid;
+  NUBasis *restrict x_basis, *restrict y_basis;
   BCtype_s xBC, yBC;
 } NUBspline_2d_s;
+
+typedef struct
+{
+  spline_code code;
+  float* restrict coefs;
+  int x_stride, y_stride;
+  NUgrid  *restrict x_grid,  *restrict y_grid,  *restrict z_grid;
+  NUBasis *restrict x_basis, *restrict y_basis, *restrict z_basis;
+  BCtype_s xBC, yBC, zBC;
+} NUBspline_3d_s;
 
 ///////////////////////////
 // Double precision real //
@@ -57,6 +67,16 @@ typedef struct
   BCtype_d xBC, yBC;
 } NUBspline_2d_d;
 
+typedef struct
+{
+  spline_code code;
+  double* restrict coefs;
+  int x_stride, y_stride;
+  NUgrid  *restrict x_grid,  *restrict y_grid,  *restrict z_grid;
+  NUBasis *restrict x_basis, *restrict y_basis, *restrict z_basis;
+  BCtype_d xBC, yBC, zBC;
+} NUBspline_3d_d;
+
 //////////////////////////////
 // Single precision complex //
 //////////////////////////////
@@ -79,6 +99,16 @@ typedef struct
   BCtype_c xBC, yBC;
 } NUBspline_2d_c;
 
+typedef struct
+{
+  spline_code code;
+  complex_float* restrict coefs;
+  int x_stride, y_stride;
+  NUgrid  *restrict x_grid,  *restrict y_grid,  *restrict z_grid;
+  NUBasis *restrict x_basis, *restrict y_basis, *restrict z_basis;
+  BCtype_c xBC, yBC, zBC;
+} NUBspline_3d_c;
+
 //////////////////////////////
 // Double precision complex //
 //////////////////////////////
@@ -86,18 +116,29 @@ typedef struct
 {
   spline_code code;
   complex_double* restrict coefs;
-  NUgrid *restrict x_grid;
+  NUgrid  *restrict x_grid;
   NUBasis *restrict x_basis;
   BCtype_z xBC;
 } NUBspline_1d_z;
+
 typedef struct
 {
   spline_code code;
   complex_double* restrict coefs;
   int x_stride;
-  NUgrid *restrict x_grid, *restrict y_grid;
+  NUgrid  *restrict x_grid,  *restrict y_grid;
   NUBasis *restrict x_basis, *restrict y_basis;
   BCtype_z xBC, yBC;
 } NUBspline_2d_z;
+
+typedef struct
+{
+  spline_code code;
+  complex_double* restrict coefs;
+  int x_stride, y_stride;
+  NUgrid  *restrict x_grid,  *restrict y_grid,  *restrict z_grid;
+  NUBasis *restrict x_basis, *restrict y_basis, *restrict z_basis;
+  BCtype_z xBC, yBC, zBC;
+} NUBspline_3d_z;
 
 #endif
