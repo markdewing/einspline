c128396be60fd8580e666e308f61b1b7af57b3c6
esler <esler@d3ca5a85-aa2c-0410-a728-ae544c1d3856>
2007-04-11 14:02:01 +0000

    Adding forgotten file containing NUB structure definitions.

---
+++ src/nubspline_structs.h
@@ -0,0 +1,67 @@
+#ifndef NUBSPLINE_STRUCTS_H
+#define NUBSPLINE_STRUCTS_H
+
+#include "nubspline_base.h"
+
+#ifdef __cplusplus
+typedef complex<float>  complex_float;
+typedef complex<double> complex_double;
+#else
+#include <complex.h>
+typedef complex float  complex_float;
+typedef complex double complex_double;
+#endif
+
+///////////////////////////
+// Single precision real //
+///////////////////////////
+typedef struct
+{
+  spline_code code;
+  float* restrict coefs;
+  int coefs_size;
+  NUgrid *x_grid;
+  NUBasis *x_basis;
+  BCtype_s xBC;
+} NUBspline_1d_s;
+
+///////////////////////////
+// Double precision real //
+///////////////////////////
+typedef struct
+{
+  spline_code code;
+  double* restrict coefs;
+  int coefs_size;
+  NUgrid *x_grid;
+  NUBasis *x_basis;
+  BCtype_d xBC;
+} NUBspline_1d_d;
+
+//////////////////////////////
+// Single precision complex //
+//////////////////////////////
+typedef struct
+{
+  spline_code code;
+  complex_float* restrict coefs;
+  int coefs_size;
+  NUgrid *x_grid;
+  NUBasis *x_basis;
+  BCtype_c xBC;
+} NUBspline_1d_c;
+
+//////////////////////////////
+// Double precision complex //
+//////////////////////////////
+typedef struct
+{
+  spline_code code;
+  complex_double* restrict coefs;
+  int coefs_size;
+  NUgrid *x_grid;
+  NUBasis *x_basis;
+  BCtype_z xBC;
+} NUBspline_1d_z;
+
+#endif
